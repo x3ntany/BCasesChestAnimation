6814da31fb33d6a0a27db46a621b354b13fd349a
x3ntany <xentany@proton.me>
2025-03-16 14:38:58 +0900

    Add a sound codec

--- src/main/java/me/xentany/bcaseschestanimation/serialization/XBukkitCodecs.java
+++ src/main/java/me/xentany/bcaseschestanimation/serialization/XBukkitCodecs.java
@@ -4,6 +4,7 @@ import blib.com.mojang.serialization.DataResult;
 import blib.com.mojang.serialization.DynamicOps;
 import blib.com.mojang.serialization.codecs.PrimitiveCodec;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.by1337.blib.configuration.serialization.BukkitCodecs;
 import org.jetbrains.annotations.NotNull;
 
@@ -25,6 +26,23 @@ public final class XBukkitCodecs extends BukkitCodecs {
 
     public <T> T write(final @NotNull DynamicOps<T> ops, final @NotNull Particle particle) {
       return ops.createString(particle.name());
+    }
+  };
+
+  public static final PrimitiveCodec<Sound> SOUND = new PrimitiveCodec<>() {
+    public <T> DataResult<Sound> read(@NotNull DynamicOps<T> ops, T t) {
+      return ops.getStringValue(t).flatMap(soundName -> {
+        try {
+          var sound = Sound.valueOf(soundName.toUpperCase(Locale.ROOT));
+          return DataResult.success(sound);
+        } catch (final IllegalArgumentException e) {
+          return DataResult.error(() -> "Invalid sound name: " + soundName);
+        }
+      });
+    }
+
+    public <T> T write(final @NotNull DynamicOps<T> ops, final @NotNull Sound sound) {
+      return ops.createString(sound.name());
     }
   };
 }
