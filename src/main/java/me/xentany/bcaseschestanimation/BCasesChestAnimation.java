1cedb714f90f1c6c0c6d5886d38e9fc913f73d27
x3ntany <xentany@proton.me>
2025-04-13 16:38:52 +0900

    Add init of SafeEnumUtil and ChestAnimationListener

--- src/main/java/me/xentany/bcaseschestanimation/BCasesChestAnimation.java
+++ src/main/java/me/xentany/bcaseschestanimation/BCasesChestAnimation.java
@@ -2,6 +2,11 @@ package me.xentany.bcaseschestanimation;
 
 import dev.by1337.bc.addon.AbstractAddon;
 import dev.by1337.bc.animation.AnimationRegistry;
+import me.xentany.bcaseschestanimation.animation.ChestAnimation;
+import me.xentany.bcaseschestanimation.animation.ChestAnimationListener;
+import me.xentany.bcaseschestanimation.particle.SafeEnumUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.event.HandlerList;
 import org.by1337.blib.util.SpacedNameKey;
 
 import java.io.File;
@@ -9,17 +14,26 @@ import java.io.File;
 @SuppressWarnings("unused") // That's the way to do it...
 public final class BCasesChestAnimation extends AbstractAddon {
 
+  private ChestAnimationListener animationListener;
+
   @Override
   protected void onEnable() {
-    AnimationRegistry.INSTANCE.register("xcommunity:chest", ChestAnimation::new);
-
     var file = new File(this.getPlugin().getDataFolder(), "animations/xcommunity/chest.yml");
 
     this.saveResourceToFile("chest.yml", file);
+
+    SafeEnumUtil.loadLogger(this.getLogger());
+
+    AnimationRegistry.INSTANCE.register("xcommunity:chest", ChestAnimation::new);
+
+    this.animationListener = new ChestAnimationListener();
+
+    Bukkit.getPluginManager().registerEvents(this.animationListener, this.getPlugin());
   }
 
   @Override
   protected void onDisable() {
     AnimationRegistry.INSTANCE.unregister(new SpacedNameKey("xcommunity:chest"));
+    HandlerList.unregisterAll(this.animationListener);
   }
 }
