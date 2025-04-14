e48f2e8ec54df06ceaf0d0ec89160ccccd101696
x3ntany <xentany@proton.me>
2025-04-14 18:56:52 +0900

    Initial commit

---
+++ src/main/java/me/xentany/bcaseschestanimation/particle/SoundUtil.java
@@ -0,0 +1,22 @@
+package me.xentany.bcaseschestanimation.particle;
+
+import org.bukkit.Sound;
+import org.jetbrains.annotations.NotNull;
+
+public final class SoundUtil {
+
+  public static @NotNull Sound getSound(final @NotNull String soundName)
+      throws NoSuchFieldException, IllegalAccessException {
+    try {
+      return Sound.valueOf(soundName);
+    } catch (final IllegalArgumentException e) {
+      var field = Sound.class.getField(soundName);
+
+      if (Sound.class.isAssignableFrom(field.getType())) {
+        return (Sound) field.get(null);
+      }
+    }
+
+    throw new NoSuchFieldException("Sound not found: " + soundName);
+  }
+}
